cfd7c423de37f41c0ba2611f5d3643c7eef93255
Nelson <nelson.mieszkalski@gmail.com>
2022-06-06 14:03:50 -0400

    Added footer and removed unused files from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Form from './components/form/Form';
+import Footer from './components/Footer';
 import './styles/App.css'
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className='app'>
         <Header />
         <Form />
+        <Footer />
       </div>
     )
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+
+    render() {
+        return (
+                <div className='footer'><a className='footer-tag' href='https://github.com/NelsonMN'>Created By: NelsonMN</a></div>
+        )
+    }
+}
+
+export default Footer
